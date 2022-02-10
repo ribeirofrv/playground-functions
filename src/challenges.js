4b54f62b5feaff9ebfbaf416d36ea593ae33b29d
Fran Ribeiro <ribeirofrv@gmail.com>
2022-02-10 20:50:17 -0300

    Cumpre Requisito 9 - Codifica e Descodifica frases

--- src/challenges.js
+++ src/challenges.js
@@ -56,11 +56,21 @@ function fizzBuzz() {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let code = phrase.replace(/a/g, '1')
+    .replace(/e/g, '2')
+    .replace(/i/g, '3')
+    .replace(/o/g, '4')
+    .replace(/u/g, '5');
+  return code;
 }
-function decode() {
-  // seu código aqui
+function decode(phrase) {
+  let code = phrase.replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+  return code;
 }
 
 // Desafio 10
